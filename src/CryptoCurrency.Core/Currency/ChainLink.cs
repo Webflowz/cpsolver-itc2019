216a99229d7b1ed92799b61d03dbd0d531899319
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-05-23 13:16:33 +0000

    PCI: Fix TI816X class code quirk

---
+++ src/CryptoCurrency.Core/Currency/ChainLink.cs
@@ -0,0 +1,12 @@
+
+namespace CryptoCurrency.Core.Currency
+{
+    public class ChainLink : ICurrency
+    {
+        public CurrencyCodeEnum Code => CurrencyCodeEnum.LINK;
+        
+        public string Label => "ChainLink";
+
+        public string Symbol => "LINK";
+    }
+}
---
+++ src/CryptoCurrency.Core/Currency/Cindicator.cs
@@ -0,0 +1,12 @@
+
+namespace CryptoCurrency.Core.Currency
+{
+    public class Cindicator : ICurrency
+    {
+        public CurrencyCodeEnum Code => CurrencyCodeEnum.CND;
+        
+        public string Label => "Cindicator";
+
+        public string Symbol => "CND";
+    }
+}
