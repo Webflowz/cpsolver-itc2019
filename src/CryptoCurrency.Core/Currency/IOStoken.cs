298a87723e498fbd27938496c328385e68de380e
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-07-05 09:40:47 +0000

    Release v3.1.1

--- src/CryptoCurrency.Core/Currency/IOStoken.cs
+++ src/CryptoCurrency.Core/Currency/IOStoken.cs
@@ -2,4 +2,10 @@ namespace CryptoCurrency.Core.Currency
 {
     public class IOStoken : ICurrency
     {
- 
+        public CurrencyCodeEnum Code => CurrencyCodeEnum.IOST;
+        
+        public string Label => "IOStoken";
+
+        public string Symbol => "IOST";
+    }
+}
---
+++ src/CryptoCurrency.Core/Currency/Iota.cs
@@ -0,0 +1,7 @@
+﻿namespace CryptoCurrency.Core.Currency
+{
+    public class Iota : ICurrency
+    {
+        public CurrencyCodeEnum Code => CurrencyCodeEnum.IOTA;
+
+        public string Label => "Iot
