951907fc3719d2aa7224c36496cc803310fd8f95
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-07-05 08:42:11 +0000

    Add test dependencies

--- src/CryptoCurrency.Core/Currency/ICurrencyFactory.cs
+++ src/CryptoCurrency.Core/Currency/ICurrencyFactory.cs
@@ -1,1 +1,13 @@
-﻿u
+﻿using System.Collections.Generic;
+
+namespace CryptoCurrency.Core.Currency
+{
+    public interface ICurrencyFactory
+    {
+        ICurrency Get(CurrencyCodeEnum code);
+
+        ICurrency Get(string code);
+
+        ICollection<ICurrency> List();
+    }
+}
---
+++ src/CryptoCurrency.Core/Currency/INSEcosystem.cs
@@ -0,0 +1,12 @@
+
+namespace CryptoCurrency.Core.Currency
+{
+    public class INSEcosystem : ICurrency
+    {
+        public CurrencyCodeEnum Code => CurrencyCodeEnum.INS;
+        
+        public string Label => "INSEcosystem";
+
+        public string Symbol => "INS";
+    }
+}
