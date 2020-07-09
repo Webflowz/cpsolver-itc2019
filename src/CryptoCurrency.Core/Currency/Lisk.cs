fcbc90c6c788007b98508ed0797ba1f168194e7e
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-07-09 18:43:02 +0000

    Add missing function

--- src/CryptoCurrency.Core/Currency/Lisk.cs
+++ src/CryptoCurrency.Core/Currency/Lisk.cs
@@ -1,1 +1,11 @@
-namespac
+namespace CryptoCurrency.Core.Currency
+{
+    public class Lisk : ICurrency
+    {
+        public CurrencyCodeEnum Code => CurrencyCodeEnum.LSK;
+        
+        public string Label => "Lisk";
+
+        public string Symbol => "LSK";
+    }
+}
