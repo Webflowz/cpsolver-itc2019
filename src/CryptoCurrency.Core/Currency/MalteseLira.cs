4d8b0fd27200ac2fb83c6bbce74c7ed8e0887aa5
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-07-26 21:20:54 +0000

    more minor updates

--- src/CryptoCurrency.Core/Currency/MalteseLira.cs
+++ src/CryptoCurrency.Core/Currency/MalteseLira.cs
@@ -2,4 +2,10 @@ namespace CryptoCurrency.Core.Currency
 {
     public class MalteseLira : ICurrency
     {
-        public CurrencyCodeEnum Code => Curren
+        public CurrencyCodeEnum Code => CurrencyCodeEnum.MTL;
+        
+        public string Label => "MalteseLira";
+
+        public string Symbol => "MTL";
+    }
+}
---
+++ src/CryptoCurrency.Core/Currency/MoedaLoyaltyPoints.cs
@@ -0,0 +1,7 @@
+namespace CryptoCurrency.Core.Currency
+{
+    public class MoedaLoyaltyPoints : ICurrency
+    {
+        public CurrencyCodeEnum Code => CurrencyCodeEnum.MDA;
+        
+  
