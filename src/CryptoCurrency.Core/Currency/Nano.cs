0fc6ff1ef209b0f9545a630932073a1f011d1376
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-07-27 11:08:29 +0000

    docs wip

---
+++ src/CryptoCurrency.Core/Currency/Nano.cs
@@ -0,0 +1,12 @@
+
+namespace CryptoCurrency.Core.Currency
+{
+    public class Nano : ICurrency
+    {
+        public CurrencyCodeEnum Code => CurrencyCodeEnum.NANO;
+        
+        public string Label => "Nano";
+
+        public string Symbol => "NANO";
+    }
+}
---
+++ src/CryptoCurrency.Core/Currency/Nebulas.cs
@@ -0,0 +1,5 @@
+namespace CryptoCurrency.Core.Currency
+{
+    public class Nebulas : ICurrency
+    {
+        public CurrencyCodeE
