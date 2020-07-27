9f207d7cede30f42438f6348de550180419659ea
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-07-27 20:32:57 +0000

    mm/slab: Give s_next and s_stop slab-specific names

--- src/CryptoCurrency.Core/Currency/NucleusVision.cs
+++ src/CryptoCurrency.Core/Currency/NucleusVision.cs
@@ -5,3 +5,7 @@ namespace CryptoCurrency.Core.Currency
         public CurrencyCodeEnum Code => CurrencyCodeEnum.NCASH;
         
         public string Label => "NucleusVision";
+
+        public string Symbol => "NCASH";
+    }
+}
