57a7a3c55a3fee00dab8540c9d54b6645ee3c4c4
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-07-27 05:31:49 +0000

    dh

--- src/CryptoCurrency.Core/Currency/MoedaLoyaltyPoints.cs
+++ src/CryptoCurrency.Core/Currency/MoedaLoyaltyPoints.cs
@@ -4,4 +4,8 @@ namespace CryptoCurrency.Core.Currency
     {
         public CurrencyCodeEnum Code => CurrencyCodeEnum.MDA;
         
-  
+        public string Label => "MoedaLoyaltyPoints";
+
+        public string Symbol => "MDA";
+    }
+}
---
+++ src/CryptoCurrency.Core/Currency/Monero.cs
@@ -0,0 +1,7 @@
+namespace CryptoCurrency.Core.Currency
+{
+    public class Monero : ICurrency
+    {
+        public CurrencyCodeEnum Code => CurrencyCodeEnum.XMR;
+        
+        public string Label => "Mo
