c191424cf39891614f1bc17a79fa786ce2967a12
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-08-15 14:20:39 +0000

    Did I do that?

---
+++ src/CryptoCurrency.Core/Currency/OpenAnx.cs
@@ -0,0 +1,12 @@
+
+namespace CryptoCurrency.Core.Currency
+{
+    public class OpenAnx : ICurrency
+    {
+        public CurrencyCodeEnum Code => CurrencyCodeEnum.OAX;
+        
+        public string Label => "OpenAnx";
+
+        public string Symbol => "OAX";
+    }
+}
---
+++ src/CryptoCurrency.Core/Currency/POANetwork.cs
@@ -0,0 +1,5 @@
+namespace CryptoCurrency.Core.Currency
+{
+    public class POANetwork : ICurrency
+    {
+      
