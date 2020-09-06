ce677610d43ba54bb1ee50e184580536aad00761
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-09-06 21:48:55 +0000

    Update gitignores

--- src/CryptoCurrency.Core/Currency/Viacoin.cs
+++ src/CryptoCurrency.Core/Currency/Viacoin.cs
@@ -1,1 +1,11 @@
-namesp
+namespace CryptoCurrency.Core.Currency
+{
+    public class Viacoin : ICurrency
+    {
+        public CurrencyCodeEnum Code => CurrencyCodeEnum.VIA;
+        
+        public string Label => "Viacoin";
+
+        public string Symbol => "VIA";
+    }
+}
