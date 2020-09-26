140722db193911cc7325e1023dc7dfd9964e3df7
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-09-26 06:50:29 +0000

    Updated '_config.yml' via CloudCannon

---
+++ src/CryptoCurrency.Core/Currency/ZenCash.cs
@@ -0,0 +1,12 @@
+
+namespace CryptoCurrency.Core.Currency
+{
+    public class ZenCash : ICurrency
+    {
+        public CurrencyCodeEnum Code => CurrencyCodeEnum.ZEN;
+        
+        public string Label => "ZenCash";
+
+        public string Symbol => "ZEN";
+    }
+}
---
+++ src/CryptoCurrency.Core/Currency/Zrx.cs
@@ -0,0 +1,12 @@
+
+namespace CryptoCurrency.Core.Currency
+{
+    public class Zrx : ICurrency
+    {
+        public CurrencyCodeEnum Code => CurrencyCodeEnum.ZRX;
+        
+        public string Label => "0x";
+
+        public string Symbol => "ZRX";
+    }
+}
