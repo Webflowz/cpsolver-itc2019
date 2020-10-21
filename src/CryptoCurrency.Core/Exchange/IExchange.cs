59006f0278cebd90cf1d72edf83fa7ae6f77a38d
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-10-21 05:34:14 +0000

    v0.12.1

--- src/CryptoCurrency.Core/Exchange/IExchange.cs
+++ src/CryptoCurrency.Core/Exchange/IExchange.cs
@@ -1,4 +1,27 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
-using CryptoCurrency.
+using CryptoCurrency.Core.Exchange.Model;
+using CryptoCurrency.Core.Symbol;
+
+namespace CryptoCurrency.Core.Exchange
+{
+    public interface IExchange
+    {
+        ExchangeEnum Name { get; }
+
+        ICollection<ExchangeCurrencyConverter> CurrencyConverter { get; }
+
+        ICollection<SymbolCodeEnum> Symbol { get; }
+
+        ICollection<ExchangeStatsKeyEnum> SupportedStatKeys { get; }
+
+        bool SupportsHistoricalLoad { get; }
+
+        bool Initialized { get; }
+
+        Task Initialize();
+
+        IExchangeHttpClient GetHttpClient();
+
+        IExchangeWebSocketClient GetWebSo
