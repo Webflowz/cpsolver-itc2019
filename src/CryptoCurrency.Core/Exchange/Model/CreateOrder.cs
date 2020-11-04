81865a8e6d8c1469fc76f689525fb19145f40431
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-11-04 06:01:34 +0000

    Issues

--- src/CryptoCurrency.Core/Exchange/Model/CreateOrder.cs
+++ src/CryptoCurrency.Core/Exchange/Model/CreateOrder.cs
@@ -1,2 +1,26 @@
 ﻿using CryptoCurrency.Core.OrderSide;
-using CryptoCurrency.Core.Order
+using CryptoCurrency.Core.OrderState;
+using CryptoCurrency.Core.OrderType;
+using CryptoCurrency.Core.Symbol;
+
+namespace CryptoCurrency.Core.Exchange.Model
+{
+    public class CreateOrder
+    {
+        public ExchangeEnum Exchange { get; set; }
+
+        public SymbolCodeEnum SymbolCode { get; set; }
+
+        public Epoch OrderEpoch { get; set; }
+
+        public string Id { get; set; }
+
+        public OrderStateEnum State { get; set; }
+
+        public OrderTypeEnum Type { get; set; }
+
+        public OrderSideEnum Side { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Vo
