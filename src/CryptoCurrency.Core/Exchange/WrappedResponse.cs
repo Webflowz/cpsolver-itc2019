ab78f4a33a3420e0ea743b1cf18f263f6540eab8
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-11-05 18:30:30 +0000

    Javadoc update

---
+++ src/CryptoCurrency.Core/Exchange/WrappedResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace CryptoCurrency.Core.Exchange
+{
+    public enum WrappedResponseStatusCode
+    {
+        Ok = 0,
+        ApiError = 2,
+        FatalError = 3
+    }
+
+    public class W
