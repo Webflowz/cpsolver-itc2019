9d9b6cfd320ccafe55c72fb4bb729c30ca218ff9
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-11-05 23:10:11 +0000

    Back to development

--- src/CryptoCurrency.Core/Exchange/WrappedResponse.cs
+++ src/CryptoCurrency.Core/Exchange/WrappedResponse.cs
@@ -7,4 +7,14 @@
         FatalError = 3
     }
 
-    public class W
+    public class WrappedResponse<T>
+    {
+        public WrappedResponseStatusCode StatusCode { get; set; }
+
+        public string ErrorCode { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public T Data { get; set; }
+    }
+}
---
+++ src/CryptoCurrency.Core/Extensions/ExchangeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+using CryptoCurrency.Core.Currency;
+using CryptoCurrency.Core.Exchange;
+using CryptoCurrency.Core.Symbol;
+
+namespace CryptoCurrency.Core.Extensions
+{
+    public static class ExchangeExtensions
+    {
+        public static void En
