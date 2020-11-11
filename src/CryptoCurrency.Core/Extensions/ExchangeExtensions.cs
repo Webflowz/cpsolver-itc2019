a2079259af26834eae96a8cec41fd865790849cc
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-11-11 17:24:37 +0000

    fix rendering

--- src/CryptoCurrency.Core/Extensions/ExchangeExtensions.cs
+++ src/CryptoCurrency.Core/Extensions/ExchangeExtensions.cs
@@ -9,4 +9,16 @@ namespace CryptoCurrency.Core.Extensions
 {
     public static class ExchangeExtensions
     {
-        public static void En
+        public static void EnsureSymbol(this IExchange ex, ISymbol symbol)
+        {
+            var match = ex.Symbol.Any(s => s == symbol.Code);
+
+            if (!match)
+                throw new ArgumentException($"Exchange {ex.Name} doesn't support {symbol.Code}");
+        }
+
+        public static string GetCurrencyCode(this IExchange ex, CurrencyCodeEnum currencyCode)
+        {
+            var currency = ex.CurrencyConverter.Where(c => c.CurrencyCode == currencyCode).FirstOrDefault();
+
+            return currency != null && currency.AltCurrencyCode != null ? currency.AltCurrencyCod
