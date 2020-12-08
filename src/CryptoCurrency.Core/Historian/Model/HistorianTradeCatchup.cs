05400c76aab7e63db839292432f307ccb4f8e368
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-12-08 13:46:36 +0000

    Few changes.

--- src/CryptoCurrency.Core/Historian/Model/HistorianTradeCatchup.cs
+++ src/CryptoCurrency.Core/Historian/Model/HistorianTradeCatchup.cs
@@ -3,4 +3,18 @@ using CryptoCurrency.Core.Symbol;
 
 namespace CryptoCurrency.Core.Historian.Model
 {
-    public class Hi
+    public class HistorianTradeCatchup
+    {
+        public ExchangeEnum Exchange { get; set; }
+
+        public SymbolCodeEnum SymbolCode { get; set; }
+
+        public Epoch EpochTo { get; set; }
+
+        public string TradeFilter { get; set; }
+        
+        public string CurrentTradeFilter { get; set; }
+
+        public int Priority { get; set; }
+    }
+}
---
+++ src/CryptoCurrency.Core/HttpProxy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace CryptoCurrency.Core
+{
+    pub
