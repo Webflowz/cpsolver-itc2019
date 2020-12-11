702fa4f88f95a74022593234f028e201070db37d
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-12-11 17:08:10 +0000

    update link in readme

--- src/CryptoCurrency.Core/Interval/Group/Day.cs
+++ src/CryptoCurrency.Core/Interval/Group/Day.cs
@@ -9,4 +9,24 @@ namespace CryptoCurrency.Core.Interval.Group
 
         public string SuffixKey => "D";
 
-        pu
+        public string SuffixLabel => "day";
+
+        public string Label => "Day";
+
+        public ICollection<int> SupportedDuration => new List<int> { 1 };
+        
+        public Interval GetInterval(IntervalKey intervalKey, Epoch epoch)
+        {
+            var ts = TimeSpan.FromDays(intervalKey.Duration);
+
+            return new Interval
+            {
+                IntervalKey = intervalKey,
+                From = epoch.RoundDown(ts),
+                To = epoch.AddSeconds(1).RoundUp(ts)
+            };
+        }
+
+        public Interval Next(Interval interval)
+        {
+      
