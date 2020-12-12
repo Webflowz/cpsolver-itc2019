e0e1413b6c4e8cfd6b8a46d8b4a36c36a9aeabb5
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-12-12 20:03:00 +0000

    Small update to README

--- src/CryptoCurrency.Core/Interval/IIntervalGroup.cs
+++ src/CryptoCurrency.Core/Interval/IIntervalGroup.cs
@@ -2,4 +2,22 @@
 
 namespace CryptoCurrency.Core.Interval
 {
-    public interface IInt
+    public interface IIntervalGroup
+    {
+        IntervalGroupEnum IntervalGroup { get; }
+
+        string SuffixKey { get; }
+
+        string SuffixLabel { get; }
+
+        string Label { get; }
+
+        ICollection<int> SupportedDuration { get; }
+
+        Interval GetInterval(IntervalKey intervalKey, Epoch epoch);
+
+        Interval Next(Interval interval);
+
+        Interval Previous(Interval interval);
+    }
+}
---
+++ src/CryptoCurrency.Core/Interval/IIntervalRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CryptoCurrency.Core.Interval
+{
+    public interface IIntervalRepository
+    {
+        Task Add(IntervalKey intervalKey);
+
+       
