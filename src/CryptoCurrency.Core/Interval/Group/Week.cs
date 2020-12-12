1263f3654605643a73ec6e2771405372fd6705b7
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-12-12 06:34:06 +0000

    FS-Cache: Increase reference of parent after registering, netfs success

--- src/CryptoCurrency.Core/Interval/Group/Week.cs
+++ src/CryptoCurrency.Core/Interval/Group/Week.cs
@@ -5,4 +5,21 @@ namespace CryptoCurrency.Core.Interval.Group
 {
     public class Week : IIntervalGroup
     {
-        public IntervalGroupEnum Inter
+        public IntervalGroupEnum IntervalGroup => IntervalGroupEnum.Week;
+
+        public string SuffixKey => "W";
+
+        public string SuffixLabel => "week";
+
+        public string Label => "Week";
+
+        public ICollection<int> SupportedDuration => new List<int> { 1 };
+        
+        public Interval GetInterval(IntervalKey intervalKey, Epoch epoch)
+        {
+            var from = epoch.DateTime.Date.AddDays(-(int)epoch.DateTime.Date.DayOfWeek + (int)DayOfWeek.Monday);
+
+            return new Interval
+            {
+                IntervalKey = intervalKey,
+  
