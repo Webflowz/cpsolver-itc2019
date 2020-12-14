830918514db66db9f836fe586cba752ca1c7b9a5
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-12-14 05:57:08 +0000

    Update export

--- src/CryptoCurrency.Core/Interval/IntervalFactory.cs
+++ src/CryptoCurrency.Core/Interval/IntervalFactory.cs
@@ -8,4 +8,20 @@ namespace CryptoCurrency.Core.Interval
     {
         private ICollection<IIntervalGroup> IntervalGroup { get; set; }
 
-        private Dictionary<IntervalGroupEnum, Dictionary<string, IntervalK
+        private Dictionary<IntervalGroupEnum, Dictionary<string, IntervalKey>> IntervalKey { get; set; }
+
+        public IntervalFactory(IEnumerable<IIntervalGroup> intervalGroup)
+        {
+            IntervalGroup = intervalGroup.ToList();
+
+            IntervalKey = new Dictionary<IntervalGroupEnum, Dictionary<string, IntervalKey>>();
+
+            foreach(var group in intervalGroup)
+            {
+                var keys = new Dictionary<string, IntervalKey>();
+
+                foreach (var duration in group.SupportedDuration)
+                {
+                    var key = $"{duration}{group.SuffixKey}";
+
+         
