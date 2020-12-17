014dc4a71c2cfe218ffcc2149e3aee742f0e1c34
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-12-17 13:24:40 +0000

    ath9k: fix BE/BK queue order

--- src/CryptoCurrency.Core/Market/MarketTrade.cs
+++ src/CryptoCurrency.Core/Market/MarketTrade.cs
@@ -8,4 +8,18 @@ namespace CryptoCurrency.Core.Market
     {
         public ExchangeEnum Exchange { get; set; }
 
-        public SymbolCodeEnu
+        public SymbolCodeEnum SymbolCode { get; set; }
+
+        public Epoch Epoch { get; set; }
+
+        public long TradeId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Volume { get; set; }
+
+        public OrderSideEnum? Side { get; set; }
+
+        public string SourceTradeId { get; set; }
+    }
+}
---
+++ src/CryptoCurrency.Core/Market/Ohlc.cs
@@ -0,0 +1,12 @@
+﻿using CryptoCurrency.Core.Exchange;
+using CryptoCurrency.Core.Symbol;
+
+namespace CryptoCurrency.Core.Market
+{
+    public class Ohlc
+    {
+        public ExchangeEnum Exchange { get; set; }
+
+        public SymbolCodeEnum Symbol { get; set; }
+
+        public string 
