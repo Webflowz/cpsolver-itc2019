6bf203f2afbd555baa1fa7f7dff6257225d34e40
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-12-17 20:39:23 +0000

    added migrations

--- src/CryptoCurrency.Core/MarketIndicator/MarketIndicatorProvider.cs
+++ src/CryptoCurrency.Core/MarketIndicator/MarketIndicatorProvider.cs
@@ -6,4 +6,22 @@ using System.Threading.Tasks;
 using static TicTacTec.TA.Library.Core;
 
 using CryptoCurrency.Core.Exchange;
-using CryptoCurre
+using CryptoCurrency.Core.Extensions;
+using CryptoCurrency.Core.Interval;
+using CryptoCurrency.Core.Market;
+using CryptoCurrency.Core.MarketIndicator.Model;
+using CryptoCurrency.Core.Symbol;
+
+namespace CryptoCurrency.Core.MarketIndicator
+{
+    public class MarketIndicatorProvider : IMarketIndicatorProvider
+    {
+        private IIntervalFactory IntervalFactory { get; set; }
+
+        private IMarketRepository MarketRepository { get; set; }
+
+        public MarketIndicatorProvider(IIntervalFactory intervalFactory, IMarketRepository marketRepository)
+        {
+            IntervalFactory = intervalFactory;
+
+           
