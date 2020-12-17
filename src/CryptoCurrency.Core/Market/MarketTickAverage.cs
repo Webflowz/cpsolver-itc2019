beb8cc594bf6b884458998e4d83ad352931a3083
Webflowz <123558440+Webflowz@users.noreply.github.com>
2020-12-17 12:36:44 +0000

    updated niche/functional

--- src/CryptoCurrency.Core/Market/MarketTickAverage.cs
+++ src/CryptoCurrency.Core/Market/MarketTickAverage.cs
@@ -5,4 +5,18 @@ using CryptoCurrency.Core.Symbol;
 
 namespace CryptoCurrency.Core.Market
 {
-   
+    public class MarketTickAverage
+    {
+        public SymbolCodeEnum SymbolCode { get; set; }
+
+        public Epoch Epoch { get; set; }
+
+        public decimal BuyPrice { get; set; }
+
+        public decimal SellPrice { get; set; }
+
+        public decimal LastPrice { get; set; }
+
+        public ICollection<ExchangeEnum> Exchanges { get; set; }
+    }
+}
---
+++ src/CryptoCurrency.Core/Market/MarketTrade.cs
@@ -0,0 +1,11 @@
+﻿using CryptoCurrency.Core.Exchange;
+using CryptoCurrency.Core.OrderSide;
+using CryptoCurrency.Core.Symbol;
+
+namespace CryptoCurrency.Core.Market
+{
+    public class MarketTrade
+    {
+        public ExchangeEnum Exchange { get; set; }
+
+        public SymbolCodeEnu
