0f9410fdc478839e2ffb0380489f5bfcdb7d8e52
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-01-20 07:28:05 +0000

    added player

--- src/CryptoCurrency.Core/Symbol/SymbolFactory.cs
+++ src/CryptoCurrency.Core/Symbol/SymbolFactory.cs
@@ -4,4 +4,20 @@ using System.Linq;
 
 using CryptoCurrency.Core.Currency;
 
-namespace Cr
+namespace CryptoCurrency.Core.Symbol
+{
+    public class SymbolFactory : ISymbolFactory
+    {
+        private ICollection<ISymbol> Symbols { get; set; }
+
+        public SymbolFactory(ICurrencyFactory currencyFactory)
+        {
+            var currencies = currencyFactory.List();
+
+            Symbols = new List<ISymbol>();
+
+            foreach(var symbolCodeEnum in Enum.GetValues(typeof(SymbolCodeEnum)))
+            {
+                var symbolCode = symbolCodeEnum.ToString();
+
+                var tradable = !symbolCode.EndsWith("SHORTS") && !symbolCode.EndsWit
