40b2981b9515a0426e82935f33bf19e63c4b0028
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-01-29 12:34:07 +0000

    cosmétique

--- src/CryptoCurrency.ExchangeClient/Binance/Http/BinanceRateLimiter.cs
+++ src/CryptoCurrency.ExchangeClient/Binance/Http/BinanceRateLimiter.cs
@@ -12,3 +12,33 @@ namespace CryptoCurrency.ExchangeClient.Binance.Http
 
         public BinanceRateLimiter()
         {
+            Count = 0;
+
+            MaxCount = 3;
+
+            FillBucket();
+        }
+
+        private void FillBucket() => Task.Run(async () =>
+        {
+            while (true)
+            {
+                await Task.Delay(1000);
+
+                if (Count > 0)
+                    Count--;
+            }
+        });
+
+        public async Task Wait()
+        {
+            while (true)
+            {
+                if (Count + 1 < MaxCount)
+                {
+                    Count++;
+
+                    return;
+                }
+
+                await T
