f0489f84b8136689e905bc1cf9cab9eb3f651359
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-01-29 07:51:48 +0000

    update CI

---
+++ src/CryptoCurrency.ExchangeClient/Binance/Http/BinanceRateLimiter.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+using CryptoCurrency.Core.RateLimiter;
+
+namespace CryptoCurrency.ExchangeClient.Binance.Http
+{
+    public class BinanceRateLimiter : IRateLimiter
+    {
+        public int Count { get; set; }
+
+        private int MaxCount { get; set; }
+
+        public BinanceRateLimiter()
+        {
