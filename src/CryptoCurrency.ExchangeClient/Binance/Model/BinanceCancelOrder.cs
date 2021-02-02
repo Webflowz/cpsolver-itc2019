df4cc991610a560c19a654b180516b6965aa4aa5
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-02-02 10:44:43 +0000

    ttm: Clear the ttm page allocated from high memory zone correctly

--- src/CryptoCurrency.ExchangeClient/Binance/Model/BinanceCancelOrder.cs
+++ src/CryptoCurrency.ExchangeClient/Binance/Model/BinanceCancelOrder.cs
@@ -4,4 +4,23 @@ namespace CryptoCurrency.ExchangeClient.Binance.Model
 {
     public class BinanceCancelOrder
     {
-        [JsonP
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+
+        [JsonProperty("orderId")]
+        public string OrderId { get; set; }
+
+        [JsonProperty("origClientOrderId")]
+        public string OrigClientOrderId { get; set; }
+
+        [JsonProperty("clientOrderId")]
+        public string ClientOrderId { get; set; }
+
+        [JsonProperty("transactTime")]
+        public long TransactTime { get; set; }
+        
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("timeInForce")]
+     
