444eb8b6616c71cc7bc62b3a078be4962ed1509f
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-02-02 19:41:01 +0000

    Update talimat

--- src/CryptoCurrency.ExchangeClient/Binance/Model/BinanceNewOrder.cs
+++ src/CryptoCurrency.ExchangeClient/Binance/Model/BinanceNewOrder.cs
@@ -12,4 +12,25 @@ namespace CryptoCurrency.ExchangeClient.Binance.Model
         [JsonProperty("qty")]
         public decimal Quantity { get; set; }
 
-        [JsonPropert
+        [JsonProperty("commission")]
+        public string Commission { get; set; }
+
+        [JsonProperty("commissionAsset")]
+        public string CommissionAsset { get; set; }
+    }
+
+    public class BinanceNewOrder
+    {
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+
+        [JsonProperty("orderId")]
+        public string OrderId { get; set; }
+        
+        [JsonProperty("clientOrderId")]
+        public string ClientOrderId { get; set; }
+        
+        [JsonProperty("transactTime")]
+        public long TransactTime { get; set; }
+
+        [Js
