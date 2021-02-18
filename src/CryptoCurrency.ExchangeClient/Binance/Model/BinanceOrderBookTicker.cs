1ebe427a0e2effe58abb0685926465b38ed9fb68
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-02-18 18:27:54 +0000

    arm64: head.S: initialise mdcr_el2 in el2_setup

--- src/CryptoCurrency.ExchangeClient/Binance/Model/BinanceOrderBookTicker.cs
+++ src/CryptoCurrency.ExchangeClient/Binance/Model/BinanceOrderBookTicker.cs
@@ -2,4 +2,21 @@
 
 namespace CryptoCurrency.ExchangeClient.Binance.Model
 {
-    public class
+    public class BinanceOrderBookTicker
+    {
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+
+        [JsonProperty("bidPrice")]
+        public decimal BidPrice { get; set; }
+
+        [JsonProperty("bidQty")]
+        public decimal BidQty { get; set; }
+
+        [JsonProperty("askPrice")]
+        public decimal AskPrice { get; set; }
+
+        [JsonProperty("askQty")]
+        public decimal AskQty { get; set; }
+    }
+}
---
+++ src/CryptoCurrency.ExchangeClient/Binance/Model/BinancePriceTicker.cs
@@ -0,0 +1,6 @@
+﻿using Newtonsoft.Json;
+
+namespace CryptoCurrency.ExchangeClient.Binance.Model
+{
+    public class BinancePriceTicker
+    {
