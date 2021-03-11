8687d8f8d0e19c1a6d7c4591f4b3d3d8304e2804
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-03-11 07:46:13 +0000

    intelli_plug: add parameter to control touch boost on/off

--- src/CryptoCurrency.ExchangeClient/Bitfinex/Bitfinex.cs
+++ src/CryptoCurrency.ExchangeClient/Bitfinex/Bitfinex.cs
@@ -1,3 +1,20 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using CryptoC
+using CryptoCurrency.Core.Currency;
+using CryptoCurrency.Core.Exchange;
+using CryptoCurrency.Core.Exchange.Model;
+using CryptoCurrency.Core.Extensions;
+using CryptoCurrency.Core.Symbol;
+
+namespace CryptoCurrency.ExchangeClient.Bitfinex
+{
+    public class Bitfinex : IExchange
+    {
+        private ICurrencyFactory CurrencyFactory { get; set; }
+
+        private ISymbolFactory SymbolFactory { get; set; }
+
+        public Bitfinex(ICurrencyFactory currencyFactory, ISymbolFactory symbolFactory)
+        {
+            CurrencyFactory = currencyFactory;
+    
