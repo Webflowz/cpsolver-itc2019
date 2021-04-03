b69d6851bbe47419dbcd2f45739d3b049c375abb
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-04-03 20:58:53 +0000

    added exercises

--- src/CryptoCurrency.ExchangeClient/Bitfinex/TypeConverter.cs
+++ src/CryptoCurrency.ExchangeClient/Bitfinex/TypeConverter.cs
@@ -50,4 +50,20 @@ namespace CryptoCurrency.ExchangeClient.Bitfinex
                     SellPrice = Convert.ToDecimal(tick[2]),
                     LastPrice = Convert.ToDecimal(tick[6])
                 };
+            }
+
+            if (typeof(T2) == typeof(TradeResult))
+            {
+                var trades = obj as JArray;
+
+                var filter = trades.Count > 0 ? Convert.ToString(trades.Max(t => t[1])) : query["start"];
+
+                var symbolCode = (SymbolCodeEnum)Enum.Parse(typeof(SymbolCodeEnum), additionalData["SymbolCode"]);
+
+                return (T2)(object)new TradeResult
+                {
+                    Exchange = exchange.Name,
+                    SymbolCode = symbolCode,
+                    Trades = trades.Select(t => new MarketTrade
+                    {
       
