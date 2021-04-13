6fb38409a06214a9e5c9dde4315a058eaceb5103
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-04-13 15:16:44 +0000

    fix(typo)

--- src/CryptoCurrency.ExchangeClient/Bitfinex/WebSocket/Client.cs
+++ src/CryptoCurrency.ExchangeClient/Bitfinex/WebSocket/Client.cs
@@ -69,3 +69,142 @@ namespace CryptoCurrency.ExchangeClient.Bitfinex.WebSocket
 
                 OnClose?.Invoke(sender, new CloseEventArgs { });
             };
+
+            Connect();
+        });
+
+        public void Connect()
+        {
+            if(WebSocketClient != null)
+                WebSocketClient.Connect();
+        }
+
+        public void BeginListenTrades(ICollection<ISymbol> symbols)
+        {
+            foreach (var symbol in symbols)
+                WebSocketClient.Send(JsonConvert.SerializeObject(new SubscribeRequest { Event = "subscribe", Channel = "trades", Symbol = $"t{symbol.Code.ToString()}" }));
+        }
+
+        public void BeginListenTicker(ICollection<ISymbol> symbols)
+        {
+            foreach (var symbol in symbols)
+                WebSocketClient.Send(JsonConvert.SerializeObject(new SubscribeRequest { Event = "subscribe", Channel = "ticker", Symbol = $"t{symbol.Code.ToString()}" }));
+        }
+
+        #region Private functionality
+        private void OnMessage(object sender, MessageEventArgs e)
+        {
+            var message = e.Data.Trim();
+
+            if (!string.IsNullOrEmpty(message))
+            {
+                if (message[0] == '{')
+                {
+                    var evt = JsonConvert.DeserializeObject<BaseEventResponse>(message);
+
+                    if (evt == null)
+                        return;
+
+                    switch (evt.Event)
+                    {
+                        case "subscribed":
+                            var subscription = JsonConvert.DeserializeObject<SubscriptionEventResponse>(message);
+
+                            switch (subscription.Channel)
+                            {
+                                case "trades":
+                                    var tradeResponse = JsonConvert.DeserializeObject<TradeEventResponse>(message);
+
+                                    Channels.Add(tradeResponse.ChanId, tradeResponse);
+
+                                    break;
+                                case "ticker":
+                                    var tickerResponse = JsonConvert.DeserializeObject<TickerEventResponse>(message);
+
+                                    Channels.Add(tickerResponse.ChanId, tickerResponse);
+
+                                    break;
+                            }
+
+                            break;
+                    }
+                }
+
+                if (message[0] == '[')
+                {
+                    var data = JsonConvert.DeserializeObject<dynamic>(message);
+
+                    if (data != null && data.Count > 0)
+                    {
+                        var channelId = (long)data[0];
+
+                        var channel = Channels[channelId];
+
+                        switch (channel.Channel)
+                        {
+                            case "trades":
+                                HandleTradeMessage((TradeEventResponse)channel, data);
+
+                                break;
+                            case "ticker":
+                                HandleTickerMessage((TickerEventResponse)channel, data);
+
+                                break;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private void HandleTradeMessage(TradeEventResponse channel, dynamic data)
+        {
+            var raw = new JArray();
+
+            if (data[1] is JArray)
+            {
+                raw = data[1];
+            }
+            else
+            {
+                var type = Convert.ToString(data[1]);
+
+                if (type != "tu")
+                    return;
+
+                raw.Add(data[2]);
+            }
+
+            var pair = Exchange.DecodeSymbol(channel.Symbol);
+
+            var symbol = SymbolFactory.Get(pair[0], pair[1]);
+
+            var additionalData = new NameValueCollection();
+            additionalData.Add("SymbolCode", symbol.Code.ToString());
+
+            var result = Exchange.ChangeType<dynamic, TradeResult>(SymbolFactory, raw, null, additionalData);
+
+            OnTradesReceived?.Invoke(this, new TradesReceivedEventArgs { Data = result });
+        }
+
+        private void HandleTickerMessage(TickerEventResponse channel, dynamic data)
+        {
+            if (data[1].GetType() != typeof(JArray))
+                return;
+
+            var raw = (data[1] as JArray).ToObject<object[]>();
+
+            var pair = Exchange.DecodeSymbol(channel.Symbol);
+
+            var symbol = SymbolFactory.Get(pair[0], pair[1]);
+
+            var additionalData = new NameValueCollection();
+            additionalData.Add("SymbolCode", symbol.Code.ToString());
+
+            var result = Exchange.ChangeType<dynamic, MarketTick>(SymbolFactory, raw, null, additionalData);
+
+            OnTickerReceived?.Invoke(this, new TickerReceivedEventArgs { Data = result });
+        }
+        #endregion
+    }
+}
---
+++ src/CryptoCurrency.ExchangeClient/Bitfinex/WebSocket/SubscribeRequest.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace CryptoCurrency.ExchangeClient.Bitfinex.WebSocket
+{
+    public class SubscribeRequest
+    {
+        [JsonProperty(PropertyName = "event")]
+        public string Event { get; set; }
+
+        [JsonProperty(PropertyName = "channel")]
+        public string Channel { get; set; }
+
+        [JsonProperty(PropertyName = "symbol")]
+        public
