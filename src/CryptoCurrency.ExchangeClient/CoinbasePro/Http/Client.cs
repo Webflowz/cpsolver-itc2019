def5f581c52416eb9921a02ebad5ad17a5fc93a9
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-04-18 11:35:54 +0000

    net: ipv6: fix wrong ping_v6_sendmsg return value

--- src/CryptoCurrency.ExchangeClient/CoinbasePro/Http/Client.cs
+++ src/CryptoCurrency.ExchangeClient/CoinbasePro/Http/Client.cs
@@ -4,4 +4,25 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Security
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+using CryptoCurrency.Core;
+using CryptoCurrency.Core.Currency;
+using CryptoCurrency.Core.Exchange;
+using CryptoCurrency.Core.Exchange.Model;
+using CryptoCurrency.Core.Extensions;
+using CryptoCurrency.Core.Market;
+using CryptoCurrency.Core.OrderType;
+using CryptoCurrency.Core.OrderSide;
+using CryptoCurrency.Core.RateLimiter;
+using CryptoCurrency.Core.Symbol;
+
+using CryptoCurrency.ExchangeClient.CoinbasePro.Model;
+
+namespace CryptoCurrency.ExchangeClient.CoinbasePro.Http
+{
+    public class Cli
