5be9306de44805cc32f2594175ea44f3da2b8857
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-05-13 22:00:58 +0000

    [API] Working on API Docs

--- src/CryptoCurrency.ExchangeClient/CoinbasePro/Model/CoinbaseProAccount.cs
+++ src/CryptoCurrency.ExchangeClient/CoinbasePro/Model/CoinbaseProAccount.cs
@@ -10,4 +10,16 @@ namespace CryptoCurrency.ExchangeClient.CoinbasePro.Model
         [JsonProperty(PropertyName = "currency")]
         public string Currency { get; set; }
 
-      
+        [JsonProperty(PropertyName = "balance")]
+        public decimal Balance { get; set; }
+
+        [JsonProperty(PropertyName = "available")]
+        public decimal Available { get; set; }
+
+        [JsonProperty(PropertyName = "hold")]
+        public decimal Hold { get; set; }
+
+        [JsonProperty(PropertyName = "profile_id")]
+        public string ProfileId { get; set; }
+    }
+}
