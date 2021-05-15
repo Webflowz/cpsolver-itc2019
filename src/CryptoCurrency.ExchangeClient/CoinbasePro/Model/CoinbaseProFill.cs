a430e61e7b92c0d2c21feb7830df7a369030d547
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-05-15 07:07:44 +0000

    release 1.1.3

--- src/CryptoCurrency.ExchangeClient/CoinbasePro/Model/CoinbaseProFill.cs
+++ src/CryptoCurrency.ExchangeClient/CoinbasePro/Model/CoinbaseProFill.cs
@@ -2,4 +2,23 @@
 
 using Newtonsoft.Json;
 
-namespace CryptoCurrency.ExchangeC
+namespace CryptoCurrency.ExchangeClient.CoinbasePro.Model
+{
+    public class CoinbaseProFill
+    {
+        [JsonProperty(PropertyName = "trade_id")]
+        public string TradeId { get; set; }
+
+        [JsonProperty(PropertyName = "product_id")]
+        public string ProductId { get; set; }
+
+        [JsonProperty(PropertyName = "price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty(PropertyName = "size")]
+        public decimal Size { get; set; }
+
+        [JsonProperty(PropertyName = "order_id")]
+        public string OrderId { get; set; }
+
+        [JsonProperty(PropertyName = 
