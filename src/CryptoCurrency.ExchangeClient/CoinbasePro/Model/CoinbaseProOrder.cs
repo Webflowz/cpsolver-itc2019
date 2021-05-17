6ae90e6ce2373361f5ae4be67d5759dfb1c4ffb9
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-05-17 19:22:18 +0000

    / ‘projects/DragonHeart_KR/index.md’

--- src/CryptoCurrency.ExchangeClient/CoinbasePro/Model/CoinbaseProOrder.cs
+++ src/CryptoCurrency.ExchangeClient/CoinbasePro/Model/CoinbaseProOrder.cs
@@ -6,4 +6,22 @@ namespace CryptoCurrency.ExchangeClient.CoinbasePro.Model
 {
     public class CoinbaseProOrder
     {
-        [
+        [JsonProperty(PropertyName = "time")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "product_id")]
+        public string ProductId { get; set; }
+
+        [JsonProperty(PropertyName = "price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty(PropertyName = "size")]
+        public decimal Size { get; set; }
+
+        [JsonProperty(PropertyName = "side")]
+        public string Side { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "
