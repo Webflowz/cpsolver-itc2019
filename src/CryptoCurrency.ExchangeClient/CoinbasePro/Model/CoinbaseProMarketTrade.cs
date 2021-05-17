bc2f021d312db358e2cc190bd2530547e468c7d9
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-05-17 18:51:36 +0000

    social

--- src/CryptoCurrency.ExchangeClient/CoinbasePro/Model/CoinbaseProMarketTrade.cs
+++ src/CryptoCurrency.ExchangeClient/CoinbasePro/Model/CoinbaseProMarketTrade.cs
@@ -7,3 +7,18 @@ namespace CryptoCurrency.ExchangeClient.CoinbasePro.Model
     public class CoinbaseProMarketTrade
     {
         [JsonProperty(PropertyName = "time")]
+        public DateTime Time { get; set; }
+
+        [JsonProperty(PropertyName = "trade_id")]
+        public long TradeId { get; set; }
+
+        [JsonProperty(PropertyName = "price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty(PropertyName = "size")]
+        public decimal Size { get; set; }
+
+        [JsonProperty(PropertyName = "side")]
+        public string Side { get; set; }
+    }
+}
---
+++ src/CryptoCurrency.ExchangeClient/CoinbasePro/Model/CoinbaseProOrder.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace CryptoCurrency.ExchangeClient.CoinbasePro.Model
+{
+    public class CoinbaseProOrder
+    {
+        [
