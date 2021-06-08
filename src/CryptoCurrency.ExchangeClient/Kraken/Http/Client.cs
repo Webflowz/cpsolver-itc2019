7cb624fec1a1ecb340aa08e5389e0ff9b46f5b26
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-06-08 13:03:08 +0000

    update files

--- src/CryptoCurrency.ExchangeClient/Kraken/Http/Client.cs
+++ src/CryptoCurrency.ExchangeClient/Kraken/Http/Client.cs
@@ -8,4 +8,23 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-using 
+using Newtonsoft.Json;
+
+using CryptoCurrency.Core;
+using CryptoCurrency.Core.Currency;
+using CryptoCurrency.Core.Exchange;
+using CryptoCurrency.Core.Exchange.Model;
+using CryptoCurrency.Core.Extensions;
+using CryptoCurrency.Core.Market;
+using CryptoCurrency.Core.OrderSide;
+using CryptoCurrency.Core.OrderType;
+using CryptoCurrency.Core.Symbol;
+using CryptoCurrency.Core.RateLimiter;
+
+using CryptoCurrency.ExchangeClient.Kraken.Model;
+
+namespace CryptoCurrency.ExchangeClient.Kraken.Http
+{
+    public class Client : IExchangeHttpClient
+    {
+        private Kraken Exchange { get; se
