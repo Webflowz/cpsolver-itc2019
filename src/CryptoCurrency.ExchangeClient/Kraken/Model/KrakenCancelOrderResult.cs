066694f49d7f32a7e506480923d5d34d3675285e
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-07-21 16:41:58 +0000

    Delete npm-debug.log

--- src/CryptoCurrency.ExchangeClient/Kraken/Model/KrakenCancelOrderResult.cs
+++ src/CryptoCurrency.ExchangeClient/Kraken/Model/KrakenCancelOrderResult.cs
@@ -5,4 +5,9 @@ namespace CryptoCurrency.ExchangeClient.Kraken.Model
     public class KrakenCancelOrderResult
     {
         [JsonProperty(PropertyName = "count")]
-  
+        public int Count { get; set; }
+
+        [JsonProperty(PropertyName = "pending")]
+        public bool? Pending { get; set; }
+    }
+}
---
+++ src/CryptoCurrency.ExchangeClient/Kraken/Model/KrakenOrderBook.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+namespace CryptoCurrency.ExchangeClient.Kraken.Model
+{
+    public class KrakenOrderBookPair
+    {
+        [JsonProperty(PropertyName = "asks")]
+        public ICollection<ICollection<decimal>> Asks { get; set; }
+
+        [JsonProperty(PropertyName = "bids")]
+        public ICollection<ICollection<decimal>> Bids { get; set; }
+    }
+
+    public class
