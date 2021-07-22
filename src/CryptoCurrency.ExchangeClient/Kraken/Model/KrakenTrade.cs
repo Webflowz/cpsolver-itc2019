cfa2b6ef6e8ed54a857220a86c88fd8ddc09888e
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-07-22 21:15:33 +0000

    In progress...

--- src/CryptoCurrency.ExchangeClient/Kraken/Model/KrakenTrade.cs
+++ src/CryptoCurrency.ExchangeClient/Kraken/Model/KrakenTrade.cs
@@ -12,3 +12,18 @@ namespace CryptoCurrency.ExchangeClient.Kraken.Model
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
+            var result = Activator.CreateInstance(objectType);
+
+            var typeKey = objectType.GenericTypeArguments[0];
+            var typeValue = objectType.GenericTypeArguments[1];
+
+            var propLast = objectType.GetRuntimeProperty(nameof(KrakenTrade<int, int>.Last));
+            var methodAdd = objectType.GetRuntimeMethod(nameof(KrakenTrade<int, int>.Add), new[] { typeKey, typeValue });
+
+            reader.Read();
+
+            while (reader.TokenType != JsonToken.EndObject)
+            {
+                var key = reader.Value.ToString();
+
+        
