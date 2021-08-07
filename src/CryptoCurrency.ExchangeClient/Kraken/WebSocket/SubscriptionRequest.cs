2f171e4f3433000de7aa3d4c7cd0e083d647a183
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-08-07 16:01:38 +0000

    Maven artifacts for The Knowledge Representation Interface version 1.1.6-SNAPSHOT

--- src/CryptoCurrency.ExchangeClient/Kraken/WebSocket/SubscriptionRequest.cs
+++ src/CryptoCurrency.ExchangeClient/Kraken/WebSocket/SubscriptionRequest.cs
@@ -1,2 +1,11 @@
 ﻿using Newtonsoft.Json;
-u
+using System.Collections.Generic;
+
+namespace CryptoCurrency.ExchangeClient.Kraken.WebSocket
+{
+    public class SubscriptionRequest : BaseEvent
+    {
+        [JsonProperty("pair")]
+        public ICollection<string> Pair { get; set; }
+    }
+}
