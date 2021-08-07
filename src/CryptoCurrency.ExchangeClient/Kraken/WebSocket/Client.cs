bd2613e5df8890bd9cc3808dfa68119538a32e80
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-08-07 11:25:38 +0000

    Release 1.3.0

--- src/CryptoCurrency.ExchangeClient/Kraken/WebSocket/Client.cs
+++ src/CryptoCurrency.ExchangeClient/Kraken/WebSocket/Client.cs
@@ -73,3 +73,133 @@ namespace CryptoCurrency.ExchangeClient.Kraken.WebSocket
 
         public void Connect()
         {
+            if (WebSocketClient != null)
+                WebSocketClient.Connect();
+        }
+
+        public void BeginListenTicker(ICollection<ISymbol> symbols)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BeginListenTrades(ICollection<ISymbol> symbols)
+        {
+            var pairs = new List<string>();
+
+            foreach (var symbol in symbols)
+            {
+                var baseCurrency = Exchange.GetCurrencyCode(symbol.BaseCurrencyCode);
+                var quoteCurrency = Exchange.GetCurrencyCode(symbol.QuoteCurrencyCode);
+
+                pairs.Add($"{baseCurrency}/{quoteCurrency}");
+            }
+
+            WebSocketClient.Send(JsonConvert.SerializeObject(new SubscriptionRequest { Event = "subscribe", Pair = pairs, Subscription = new BaseEventInnerRequest { Name = "trade" } }));
+        }
+        
+        public void SetApiAccess(string privateKey, string publicKey, string passphrase)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Private functionality
+        private void OnMessage(object sender, MessageEventArgs e)
+        {
+            var message = e.Data.Trim();
+
+            if (!string.IsNullOrEmpty(message))
+            {
+                if (message[0] == '{')
+                {
+                    var evt = JsonConvert.DeserializeObject<BaseEvent>(message);
+
+                    if (evt == null)
+                        return;
+
+                    switch (evt.Event)
+                    {
+                        case "subscriptionStatus":
+                            var response = JsonConvert.DeserializeObject<SubscriptionEventResponse>(message);
+
+                            switch(response.Status)
+                            {
+                                case "subscribed":
+                                    switch (response.Subscription.Name)
+                                    {
+                                        case "trade":
+                                            Channels.Add(response.ChannelId, response);
+
+                                            break;
+                                    }
+
+                                    break;
+                                case "unsubscribed":
+                                    Channels.Remove(response.ChannelId);
+
+                                    break;
+                            }
+
+                            break;
+                    }
+                }
+
+                if (message[0] == '[')
+                {
+                    var data = JsonConvert.DeserializeObject<dynamic>(message);
+
+                    if (data != null && data.Count > 0)
+                    {
+                        var channelId = (long)data[0];
+
+                        var channel = Channels[channelId];
+
+                        switch (channel.Subscription.Name)
+                        {
+                            case "trade":
+                                HandleTradeMessage(channel, data);
+
+                                break;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private void HandleTradeMessage(SubscriptionEventResponse channel, dynamic data)
+        {
+            if (!(data[1] is JArray))
+                return;
+
+            var raw = data[1] as JArray;
+
+            var pair = channel.Pair.Split('/');
+
+            var baseCurrencyCode = Exchange.GetStandardisedCurrencyCode(CurrencyFactory, pair[0]);
+            var quoteCurrencyCode = Exchange.GetStandardisedCurrencyCode(CurrencyFactory, pair[1]);
+
+            var symbol = SymbolFactory.Get(baseCurrencyCode, quoteCurrencyCode);
+
+            var trades = raw.Select(t => new MarketTrade
+            {
+                Exchange = Exchange.Name,
+                SymbolCode = symbol.Code,
+                Epoch = Epoch.FromSeconds(Convert.ToDouble(t[2])),
+                Price = Convert.ToDecimal(t[0]),
+                Volume = Convert.ToDecimal(t[1]),
+                Side = (string)t[3] == "b" ? OrderSideEnum.Buy : OrderSideEnum.Sell
+            }).ToList();
+
+            var tradeResult = new TradeResult
+            {
+                Exchange = Exchange.Name,
+                SymbolCode = symbol.Code,
+                Filter = null,
+                Trades = trades
+            };
+
+            OnTradesReceived?.Invoke(this, new TradesReceivedEventArgs { Data = tradeResult });
+        }
+        #endregion
+    }
+}
