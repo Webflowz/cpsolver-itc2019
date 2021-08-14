ae2863d198dac09c6382a6cfc14e6f10d7f06993
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-08-14 05:40:08 +0000

    login screen

---
+++ src/CryptoCurrency.HistorianService/Extension/LoggerExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+using CryptoCurrency.Core.Exchange;
+using CryptoCurrency.Core.Symbol;
+
+namespace CryptoCurrency.HistorianService.Extension
+{
+    public static class LoggerExtension
+    {
+        public static IDisposable BeginEx
