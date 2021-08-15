f11abcfb32859e29eb10adf9a1a3671f389f42cf
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-08-15 17:34:55 +0000

    ftrace: Synchronize setting function_trace_op with ftrace_trace_function

--- src/CryptoCurrency.HistorianService/Program.cs
+++ src/CryptoCurrency.HistorianService/Program.cs
@@ -8,4 +8,22 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-using Crypto
+using CryptoCurrency.Core;
+using CryptoCurrency.Core.Exchange;
+using CryptoCurrency.Core.Historian;
+using CryptoCurrency.ExchangeClient;
+using CryptoCurrency.HistorianService.Provider;
+using CryptoCurrency.Repository;
+using CryptoCurrency.Repository.Extension;
+using CryptoCurrency.Repository.Logging;
+
+namespace CryptoCurrency.HistorianService
+{
+    using Worker;
+
+    class Program
+    {
+        private static ManualResetEvent ResetEvent = new ManualResetEvent(false);
+
+        static async Task Main(string[] args)
+        
