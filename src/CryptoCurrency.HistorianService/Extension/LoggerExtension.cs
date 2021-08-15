32b094f6a7d29ed409feaf6ca4ca5af3dc8fb46e
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-08-15 13:37:03 +0000

    delete old version

--- src/CryptoCurrency.HistorianService/Extension/LoggerExtension.cs
+++ src/CryptoCurrency.HistorianService/Extension/LoggerExtension.cs
@@ -9,4 +9,19 @@ namespace CryptoCurrency.HistorianService.Extension
 {
     public static class LoggerExtension
     {
-        public static IDisposable BeginEx
+        public static IDisposable BeginExchangeScope(this ILogger logger, ExchangeEnum exchange)
+        {
+            return logger.BeginScope(new { Exchange = exchange });
+        }
+
+        public static IDisposable BeginWorkerScope(this ILogger logger, string worker)
+        {
+            return logger.BeginScope(new { Worker = worker });
+        }
+
+        public static IDisposable BeginSymbolScope(this ILogger logger, SymbolCodeEnum symbolCode)
+        {
+            return logger.BeginScope(new { SymbolCode = symbolCode });
+        }
+
+        public static IDisposable BeginExchangeStatsScope(this ILogger logger, ExchangeStatsKe
