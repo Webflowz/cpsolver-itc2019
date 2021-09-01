ca6a67d19cac2c912f96b72e7cfee02d97af5084
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-09-01 12:20:02 +0000

    Upgrade bazlets to latest stable-2.14

--- src/CryptoCurrency.HistorianService/Worker/ExchangeTradeCatchupWorker.cs
+++ src/CryptoCurrency.HistorianService/Worker/ExchangeTradeCatchupWorker.cs
@@ -1,3 +1,22 @@
 ﻿using System;
 using System.Linq;
-us
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using CryptoCurrency.Core;
+using CryptoCurrency.Core.Historian;
+using CryptoCurrency.Core.Historian.Model;
+using CryptoCurrency.Core.Exchange;
+using CryptoCurrency.Core.Symbol;
+using CryptoCurrency.Core.StorageTransaction;
+
+using CryptoCurrency.HistorianService.Extension;
+using CryptoCurrency.HistorianService.Provider;
+
+using CryptoCurrency.Repository.Edm.Historian;
+using CryptoCurrency.Core.Exchange.Model;
+
+namespace CryptoCurrency.HistorianService.Worker
+{
+    public class Exchang
