65b9fddfe90634693aee5ceb31e5bbf771d26864
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-10-09 06:49:52 +0000

    Create solution.py

--- src/CryptoCurrency.Repository/Edm/Historian/ExchangeEntity.cs
+++ src/CryptoCurrency.Repository/Edm/Historian/ExchangeEntity.cs
@@ -1,2 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
-us
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CryptoCurrency.Repository.Edm.Historian
+{
+    [Table("exchange")]
+    public class ExchangeEntity
+    {
+        [Key]
+        [Column("exchange_id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ src/CryptoCurrency.Repository/Edm/Historian/ExchangeSymbolEntity.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CryptoCurrency.Repository.Edm.Historian
+{
+    [Table("exchange_symbol")]
+    public class ExchangeSymbolEntity
+    {
+      
