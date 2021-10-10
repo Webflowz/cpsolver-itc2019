8adf300a1c3df1f61036badfa52a5095bbd477a9
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-10-10 06:54:27 +0000

    working again

--- src/CryptoCurrency.Repository/Edm/Historian/HistorianDbContext.cs
+++ src/CryptoCurrency.Repository/Edm/Historian/HistorianDbContext.cs
@@ -3,4 +3,20 @@ using Microsoft.Extensions.Logging;
 
 namespace CryptoCurrency.Repository.Edm.Historian
 {
-    
+    public class HistorianDbContext : DbContext
+    {
+        private ILoggerFactory LoggerFactory { get; set; }
+        
+        public DbSet<CurrencyEntity> Currency { get; set; }
+
+        public DbSet<SymbolEntity> Symbol { get; set; }
+
+        public DbSet<ExchangeEntity> Exchange { get; set; }
+        
+        public DbSet<ExchangeSymbolEntity> ExchangeSymbol { get; set; }
+        
+        public DbSet<ExchangeTradeEntity> ExchangeTrade { get; set; }
+
+        public DbSet<ExchangeTradeStatEntity> ExchangeTradeStat { get; set; }
+
+        public DbSet<Exchange
