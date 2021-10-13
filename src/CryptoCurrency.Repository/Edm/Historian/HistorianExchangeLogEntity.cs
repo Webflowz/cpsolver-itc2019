1abdde337c457dd522c250cb2b559aed82a56cea
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-10-13 21:13:40 +0000

    Remove stray print

--- src/CryptoCurrency.Repository/Edm/Historian/HistorianExchangeLogEntity.cs
+++ src/CryptoCurrency.Repository/Edm/Historian/HistorianExchangeLogEntity.cs
@@ -4,4 +4,28 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace CryptoCurrency.Repository.Edm.Historian
 {
     [Table("historian_exchange_log")]
-    public class HistorianEx
+    public class HistorianExchangeLogEntity
+    {
+        [Key]
+        [Column("log_id")]
+        public int Id { get; set; }
+
+        [Column("timestamp")]
+        public long Timestamp { get; set; }
+
+        [Column("log_level_id")]
+        public int LevelId { get; set; }
+
+        [Column("category")]
+        public string Category { get; set; }
+
+        [Column("message")]
+        public string Message { get; set; }
+
+        [Column("exception")]
+        public string Exception { get; set; }
+
+        [Column("exchange_id")]
+        public int? ExchangeId { get; set; }
+
+        
