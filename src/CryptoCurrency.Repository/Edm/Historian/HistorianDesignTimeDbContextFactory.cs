6b71ed3ef84a419db2022ec861f79c0098adcf37
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-10-13 11:04:51 +0000

    Fixup tests.

--- src/CryptoCurrency.Repository/Edm/Historian/HistorianDesignTimeDbContextFactory.cs
+++ src/CryptoCurrency.Repository/Edm/Historian/HistorianDesignTimeDbContextFactory.cs
@@ -9,4 +9,18 @@ namespace CryptoCurrency.Repository.Edm.Historian
     {
         private IOptions<DbContextConfigurationOptions> Configuration { get; set; }
 
-        private ILoggerFactory LoggerFactory { ge
+        private ILoggerFactory LoggerFactory { get; set; }
+
+        private DbContextOptions<HistorianDbContext> Options { get; set; }
+
+        public HistorianDesignTimeDbContextFactory(IOptions<DbContextConfigurationOptions> configuration, ILoggerFactory loggerFactory)
+        {
+            Configuration = configuration;
+
+            LoggerFactory = loggerFactory;
+
+            var builder = new DbContextOptionsBuilder<HistorianDbContext>();
+
+            var connectionString = Configuration.Value.HistorianConnectionString;
+
+            builder.UseLoggerFactory(LoggerFa
