1f7ef7c49cf93e2ad724e4f52f8ea5bcbf36a7f5
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-10-17 17:37:55 +0000

    tinydb refactoring

--- src/CryptoCurrency.Repository/Edm/Historian/IntervalKeyEntity.cs
+++ src/CryptoCurrency.Repository/Edm/Historian/IntervalKeyEntity.cs
@@ -1,21 +1,22 @@
+
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CryptoCurrency.Repository.Edm.Historian
 {
-    [Table("interval")]
-    public class IntervalEntity
+    [Table("interval_key")]
+    public class IntervalKeyEntity
     {
-        [Required]
+        [Key]
         [Column("interval_key")]
         public string IntervalKey { get; set; }
 
         [Required]
-        [Column("from_timestamp")]
-        public long FromTimestamp { get; set; }
+        [Column("interval_group_id")]
+        public int IntervalGroupId { get; set; }
 
         [Required]
-        [Column("to_timestamp")]
-        public long ToTimestamp { get; set; }
+        [Column("label")]
+        public string Label { get; set; }
     }
 }
---
+++ src/CryptoCurrency.Repository/ExchangeRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Logging;
+
+using CryptoCurrency.Core.Exchange;
+using CryptoCurrency.Core.Symbol;
+
+using CryptoCurrency.Repository.Edm.Historian;
+using System.Threading.Tasks;
+
+namespace CryptoCurrency.Reposit
