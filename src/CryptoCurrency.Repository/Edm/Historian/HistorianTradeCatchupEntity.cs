3d7c3dadda23683848873464fb23fff0f13aa787
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-10-17 08:46:31 +0000

    Prepared versions for release

--- src/CryptoCurrency.Repository/Edm/Historian/HistorianTradeCatchupEntity.cs
+++ src/CryptoCurrency.Repository/Edm/Historian/HistorianTradeCatchupEntity.cs
@@ -11,4 +11,16 @@ namespace CryptoCurrency.Repository.Edm.Historian
         [Column("symbol_id")]
         public int SymbolId { get; set; }
 
-        [Column("tr
+        [Column("trade_filter")]
+        public string TradeFilter { get; set; }
+
+        [Column("timestamp_to")]
+        public long TimestampTo { get; set; }
+
+        [Column("current_trade_filter")]
+        public string CurrentTradeFilter { get; set; }
+
+        [Column("priority")]
+        public int Priority { get; set; }
+    }
+}
---
+++ src/CryptoCurrency.Repository/Edm/Historian/IntervalEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CryptoCurrency.Repository.Edm.Historian
+{
+    [Table("interval")]
+    public class IntervalEntity
+    {
+        [Required]
+        [Column("interval
