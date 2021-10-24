7ced7f3dc56477de1e014a762a0e176d6963abd7
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-10-24 07:59:15 +0000

    mm: compaction: cache if a pageblock was scanned and no pages were isolated

--- src/CryptoCurrency.Repository/Logging/HistorianLogger.cs
+++ src/CryptoCurrency.Repository/Logging/HistorianLogger.cs
@@ -5,4 +5,21 @@ using System.Reflection;
 
 using Microsoft.Extensions.Logging;
 
-using 
+using CryptoCurrency.Core;
+using CryptoCurrency.Core.Historian.Model;
+using CryptoCurrency.Core.Exchange;
+using CryptoCurrency.Core.Symbol;
+
+namespace CryptoCurrency.Repository.Logging
+{
+    public class HistorianLogger : ILogger
+    {
+        private HistorianLoggerProvider LoggerProvider { get; set; }
+
+        private string CategoryName { get; set; }
+
+        private Dictionary<string, object> State { get; set; }
+        
+        public HistorianLogger(HistorianLoggerProvider loggerProvider, string categoryName)
+        {
+     
