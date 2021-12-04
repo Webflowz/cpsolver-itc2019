f2fa3c78dba34e6c454dca3887effe5b9df10c16
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-12-04 14:50:31 +0000

    informes

--- src/CryptoCurrency.Repository/MarketRepository.cs
+++ src/CryptoCurrency.Repository/MarketRepository.cs
@@ -459,4 +459,9 @@ namespace CryptoCurrency.Repository
                 sql += @"
                 on duplicate key update
 	                `open` = case when values(`open_timestamp`) < `open_timestamp` then values(`open`) else `open` end,
-	                `open_timestamp` = case when values(`open_timestamp
+	                `open_timestamp` = case when values(`open_timestamp`) < `open_timestamp` then values(`open_timestamp`) else `open_timestamp` end,
+                    `high` = case when values(`high`) > `high` then values(`high`) else `high` end,
+                    `low` = case when values(`low`) < `low` then values(`low`) else `low` end,
+                    `close` = case when values(`close_timestamp`) > `close_timestamp` then values(`close`) else `close` end,
+	                `close_timestamp` = case when values(`close_timestamp`) > `close_timestamp` then values(`close_timestamp`) else `close_timestamp` end,
+	                `buy_volume` = case
