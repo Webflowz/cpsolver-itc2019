c56205fb4ab2cebc36cac94c51411c8d1f96cfa0
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-12-11 09:48:45 +0000

    v0.3.4

--- src/CryptoCurrency.Repository/SymbolRepository.cs
+++ src/CryptoCurrency.Repository/SymbolRepository.cs
@@ -1,1 +1,18 @@
-﻿using System.
+﻿using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using CryptoCurrency.Core.Symbol;
+using CryptoCurrency.Repository.Edm.Historian;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CryptoCurrency.Repository
+{
+    public class SymbolRepository : ISymbolRepository
+    {
+        private ILoggerFactory LoggerFactory { get; set; }
+
+        private IDesignTimeDbContextFactory<HistorianDbContext> ContextFactory { get; set; }
+
+        public SymbolRepository(ILoggerFactory loggerFactory, IDesignTimeDbContextFactory<HistorianDbContext> contextFactory)
+       
