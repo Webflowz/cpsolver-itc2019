93554cf4024e82c19d27a9049eaa3aad7b2c2782
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-12-12 22:58:37 +0000

    heroku-automated-project-analysis (adding libcsv v1.0.1)

--- tests/integration/CryptoCurrency.ExchangeClient.Tests/BitfinexWebSocketClientTests.cs
+++ tests/integration/CryptoCurrency.ExchangeClient.Tests/BitfinexWebSocketClientTests.cs
@@ -9,4 +9,24 @@ using CryptoCurrency.Core.Symbol;
 namespace CryptoCurrency.ExchangeClient.Tests
 {
     [TestFixture]
-    public class BitfinexWebSocketClientTes
+    public class BitfinexWebSocketClientTests
+    {
+        private ICurrencyFactory CurrencyFactory { get; set; }
+
+        private ISymbolFactory SymbolFactory { get; set; }
+
+        private IExchange Exchange { get; set; }
+
+        private ExchangeWebSocketClientTests WebSocketTest { get; set; }
+
+        [SetUp]
+        protected async Task SetUp()
+        {
+            CurrencyFactory = CommonMock.GetCurrencyFactory();
+            SymbolFactory = CommonMock.GetSymbolFactory();
+
+            Exchange = new Bitfinex.Bitfinex(CurrencyFactory, SymbolFactory);
+
+            await Exchange.Initialize();
+
+            WebSocketTes
