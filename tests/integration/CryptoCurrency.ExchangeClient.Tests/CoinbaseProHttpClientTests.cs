f2ca27d3303e1fad88e5ceb585031a3d845f7167
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-12-13 10:46:26 +0000

    Added Logging

--- tests/integration/CryptoCurrency.ExchangeClient.Tests/CoinbaseProHttpClientTests.cs
+++ tests/integration/CryptoCurrency.ExchangeClient.Tests/CoinbaseProHttpClientTests.cs
@@ -9,4 +9,23 @@ using CryptoCurrency.Core.Currency;
 namespace CryptoCurrency.ExchangeClient.Tests
 {
     [TestFixture]
-    public class Co
+    public class CoinbaseProHttpClientTests
+    {
+        private ICurrencyFactory CurrencyFactory { get; set; }
+
+        private ISymbolFactory SymbolFactory { get; set; }
+
+        private IExchange Exchange { get; set; }
+
+        [SetUp]
+        protected async Task SetUp()
+        {
+            CurrencyFactory = CommonMock.GetCurrencyFactory();
+            SymbolFactory = CommonMock.GetSymbolFactory();
+
+            Exchange = new CoinbasePro.CoinbasePro(CurrencyFactory, SymbolFactory);
+
+            await Exchange.Initialize();
+        }
+
+        [T
