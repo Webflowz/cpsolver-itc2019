ec98618556d342c86f92a3f4c2f97fabef9ffdfa
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-12-16 08:14:37 +0000

    cgroup: fix an off-by-one bug which may trigger BUG_ON()

---
+++ tests/integration/CryptoCurrency.ExchangeClient.Tests/ExchangeWebSocketClientTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CryptoCurrency.Core.Exchange;
+using CryptoCurrency.Core.Symbol;
+using CryptoCurrency.Core.Currency;
+
+namespace CryptoCurrency.ExchangeClient.Tests
+{
+    public class ExchangeWebSocketClientTests
+    {
+        private ISymbolFactory SymbolFactory { get; set; }
+
+        private IExchange Exchange { get; set; }
+
+        public ExchangeWebSocketClientTests(IExchange exchange)
+        {
+            Exchange = exchan
