4e2623cf3d18480db0e8b9bf66ca4a557298b28b
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-12-18 13:44:12 +0000

    Changes to .gitignore

--- tests/integration/CryptoCurrency.ExchangeClient.Tests/KrakenWebSocketClientTests.cs
+++ tests/integration/CryptoCurrency.ExchangeClient.Tests/KrakenWebSocketClientTests.cs
@@ -8,4 +8,21 @@ using CryptoCurrency.Core.Symbol;
 
 namespace CryptoCurrency.ExchangeClient.Tests
 {
- 
+    [TestFixture]
+    public class KrakenWebSocketClientTests
+    {
+        private ICurrencyFactory CurrencyFactory { get; set; }
+
+        private ISymbolFactory SymbolFactory { get; set; }
+
+        private IExchange Exchange { get; set; }
+
+        private ExchangeWebSocketClientTests WebSocketTest { get; set; }
+
+        [SetUp]
+        protected async Task SetUp()
+        {
+            CurrencyFactory = CommonMock.GetCurrencyFactory();
+            SymbolFactory = CommonMock.GetSymbolFactory();
+
+            Exchange = new Kraken.Kraken(CurrencyFactory, S
