d5bc6a9f82da81c7691c3180f954af5d851f175d
Webflowz <123558440+Webflowz@users.noreply.github.com>
2021-12-26 09:38:32 +0000

    name changes

---
+++ tests/unit/CryptoCurrency.Core.Tests/CurrencyFactoryTests.cs
@@ -0,0 +1,29 @@
+
+﻿using System;
+using NUnit.Framework;
+
+using CryptoCurrency.Core.Currency;
+
+namespace CryptoCurrency.Core.Tests
+{
+    [TestFixture]
+    public class CurrencyFactoryTests
+    {
+        private ICurrencyFactory CurrencyFactory { get; set; }
+        
+        [SetUp]
+        protected void SetUp()
+        {
+            CurrencyFactory = CommonMock.GetCurrencyFactory();
+        }
+
+        [Test]
+        public void ContainsAllCurrencies()
+        {
+            var currencyCount = Enum.GetValues(typeof(CurrencyCodeEnum)).Length;
+            var resolvedCurrencies = CurrencyFactory.List().Count;
+
+            Assert.AreEqual(currencyCount, resolvedCurrencies);
+        }
+    }
+}
