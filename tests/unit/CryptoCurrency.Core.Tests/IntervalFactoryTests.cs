6f0ee6511dd63e7122fe33bd716ffb527920537a
Webflowz <123558440+Webflowz@users.noreply.github.com>
2023-04-01 17:45:21 +0000

    Fix licenses

--- tests/unit/CryptoCurrency.Core.Tests/IntervalFactoryTests.cs
+++ tests/unit/CryptoCurrency.Core.Tests/IntervalFactoryTests.cs
@@ -12,4 +12,22 @@ namespace CryptoCurrency.Core.Tests
     [TestFixture]
     public class IntervalFactoryTests
     {
-        private II
+        private IIntervalFactory IntervalFactory { get; set; }
+        
+        [SetUp]
+        protected void SetUp()
+        {
+            var groups = new List<IIntervalGroup>();
+            groups.Add(new Minute());
+            groups.Add(new Hour());
+            groups.Add(new Day());
+            groups.Add(new Week());
+            groups.Add(new Month());
+
+            IntervalFactory = new IntervalFactory(groups);
+        }
+
+        [Test]
+        public void GeneratesExpectedIntervalCountFor1d()
+        {
+            var from = new Epoch(new DateTime(2018, 1, 1, 0, 30, 0, Da
