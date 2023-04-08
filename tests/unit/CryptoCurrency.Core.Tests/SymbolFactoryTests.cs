c7171d15c003a71fe298c79e8bf3c31c291bef6a
Webflowz <123558440+Webflowz@users.noreply.github.com>
2023-04-08 09:24:41 +0000

    Fixes

--- tests/unit/CryptoCurrency.Core.Tests/SymbolFactoryTests.cs
+++ tests/unit/CryptoCurrency.Core.Tests/SymbolFactoryTests.cs
@@ -4,4 +4,26 @@ using System.Collections.Generic;
 
 using NUnit.Framework;
 
-using CryptoCurrency.Core.Symbol
+using CryptoCurrency.Core.Symbol;
+using CryptoCurrency.Core.Currency;
+using System.Linq;
+
+namespace CryptoCurrency.Core.Tests
+{
+    [TestFixture]
+    public class SymbolFactoryTests
+    {
+        private ICurrencyFactory CurrencyFactory { get; set; }
+
+        private ISymbolFactory SymbolFactory { get; set; }
+
+        [SetUp]
+        protected void SetUp()
+        {
+            var currency = new List<ICurrency>();
+
+            currency.Add(new Bitcoin());
+            currency.Add(new Litecoin());
+            currency.Add(new Ethereum());
+            currency.Add(new EthereumClassic());
+      
