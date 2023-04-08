5c85881472a9124601ff4ca6fbfbd4740e9bcf81
Webflowz <123558440+Webflowz@users.noreply.github.com>
2023-04-08 20:58:57 +0000

    [ci deploy]

--- tests/unit/CryptoCurrency.Core.Tests/SymbolFactoryTests.cs
+++ tests/unit/CryptoCurrency.Core.Tests/SymbolFactoryTests.cs
@@ -26,4 +26,18 @@ namespace CryptoCurrency.Core.Tests
             currency.Add(new Litecoin());
             currency.Add(new Ethereum());
             currency.Add(new EthereumClassic());
-      
+            currency.Add(new Ripple());
+            currency.Add(new Aud());
+            currency.Add(new Eur());
+            currency.Add(new Usd());
+            currency.Add(new Iota());
+            currency.Add(new Neo());
+            currency.Add(new Dash());
+            currency.Add(new Tether());
+            currency.Add(new StellarLumens());
+            currency.Add(new BinanceCoin());
+            currency.Add(new Monero());
+            currency.Add(new EOS());
+            currency.Add(new Zcash());
+            currency.Add(new TRON());
+            currency
